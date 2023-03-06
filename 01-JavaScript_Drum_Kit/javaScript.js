5f8a4a381cde221ad5ea316e9025332212fcedb0
Cristofher Pablo <chatcomchris@gmail.com>
2023-03-05 21:54:30 -0300

    Feat: add transition animation and remove it after the click

--- 01-JavaScript_Drum_Kit/javaScript.js
+++ 01-JavaScript_Drum_Kit/javaScript.js
@@ -1,8 +1,18 @@
-window.addEventListener('keydown',function(e){
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if(!audio) return; //ending the function if pressed an invalid key.
-    audio.currentTime = 0; // rewind to the start
-    audio.play();
-    key.classList.add("playing");
-});
+function soundKeys(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return; //ending the function if pressed an invalid key.
+  audio.currentTime = 0; // rewind to the start
+  audio.play();
+  key.classList.add("playing");
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
+}
+
+const keys = Array.from(document.querySelectorAll(".key"));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", soundKeys);
